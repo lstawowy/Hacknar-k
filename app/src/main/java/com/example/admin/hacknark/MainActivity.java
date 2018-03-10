8fa96a0b6ffe8394228fe3aafba08b4ccb7d6ac0
Jakub Kleszcz <jak.kleszcz@gmail.com>
2018-03-10 14:28:01 +0100

    Relacje z przycisków w MENU

--- app/src/main/java/com/example/admin/hacknark/MainActivity.java
+++ app/src/main/java/com/example/admin/hacknark/MainActivity.java
@@ -4,20 +4,37 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean userIsLogged;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        this.userIsLogged = false;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    class goSettings implements View.OnClickListener {
-        public void onClick(View v) {
-            Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
-            startActivity(intent);
+    public void JumpTo(View View)
+    {
+        String button_text;
+        button_text =((Button)View).getText().toString();
+        if(button_text.equals("Profil"))
+        {
+            Intent jump = new Intent(this,Profile.class);
+            startActivity(jump);
+        }else if(button_text.equals("Ranking")){
+            Intent jump = new Intent(this,Profile.class);
+            startActivity(jump);
         }
+/*        else if (button_text.equals("click third activity"))
+        {
+            Intent mass = new Intent(this,ThirdActivity.class);
+            startActivity(mass);
+
+        }*/
     }
 
 
--- app/src/main/java/com/example/admin/hacknark/Profile.java
+++ app/src/main/java/com/example/admin/hacknark/Profile.java
@@ -1,11 +1,13 @@
 package com.example.admin.hacknark;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class Profile extends AppCompatActivity {
 
@@ -28,4 +30,16 @@ public class Profile extends AppCompatActivity {
         */
     }
 
+    public void JumpTo(View View) {
+        String button_text;
+        button_text = ((Button) View).getText().toString();
+        if (button_text.equals("Profil")) {
+            Intent jump = new Intent(this, Profile.class);
+            startActivity(jump);
+        } else if (button_text.equals("Ranking")) {
+            Intent jump = new Intent(this, Profile.class);
+            startActivity(jump);
+        }
+
+    }
 }
