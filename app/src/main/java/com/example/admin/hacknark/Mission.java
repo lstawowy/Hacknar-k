77877ef9da81156d8bb919077bfb6746bf06b398
Łukasz Stawowy <l.w.stawowy@gmail.com>
2018-03-11 09:06:17 +0100

    Mission

--- app/src/main/java/com/example/admin/hacknark/Mission.java
+++ app/src/main/java/com/example/admin/hacknark/Mission.java
@@ -33,6 +33,10 @@ public class Mission {
                 this.missionDescription = "Dawno dawno temu w lochach pewnego palacu, zamożny czarnoksiężnik przechowywał swe skarby. Chętnych aby zdobyć bogactwo nigdy nie brakowało, choć szczęście dopisało jedynie pewnej krakowiance. Podążyła ona do Krzysztoforów za kogutem niknącym w lochach, a tam ptak przybrał postać diabła, strzegącego skarbu. Pozwolił jej zabrać tyle złota, ile zdoła sama unieść, przestrzegł jednak, by aż do wyjścia nie oglądała się za siebie, w przeciwnym razie wszystko straci. Dziewczyna, będąc już na ostatnim stopniu obejrzała się, czy przypadkiem czart jej nie ściga. Wtedy ciężkie drewniane wrota zatrzasnęły się z hukiem. I takim sposobem po dziś dzień gdzieś w lochach pałacu znajduje się ogromny skarb czekający na szczęśliwego śmiałka.";
                 this.tasks.add(new MissionTask("Znajdź pałac o którym mowa w historii",
                         50.062920,19.936729, MissionTask.TaskType.SEARCH));
+
+
+                //Tworzenie eventów na mapce ->
+                //createPUT("50.062920","19.936729","id","label","description");
                 break;
             case 3:
                 this.name = "Dwie wieże";
@@ -44,7 +48,6 @@ public class Mission {
                 this.name = "o Wandzie";
                 this.missionDescription = "Księżniczka Wanda, która nie chciała poślubić germańskiego księcia Rydygiera, by nie oddawać kraju pod obce panowanie. Książę zagroził, że najedzie ziemie Wandy, ale płomienna mowa wygłoszona przez księżniczkę sprawiła, że wróg odstąpił od planu zbrojnego. Upokorzony książę odebrał sobie życie. Wanda była jednak smutna, bo wiedziała, że musi złożyć bogom ofiarę dziękczynną.";
                 this.tasks.add(new MissionTask("znajdź miejsce upamiętniające księżniczkę Wandę", 50.070300, 20.068052,MissionTask.TaskType.SEARCH));
-
                 break;
             case 5:
                 this.name = "Zaczarowane gołębie";
@@ -68,6 +71,174 @@ public class Mission {
         }
     }
 
+
+
+    public class PUTThread extends Thread {
+        public String latitude;
+        public String longitude;
+        public String priority;
+        public String id;
+        public String label;
+        public String icon;
+        public String description;
+        public String attachmentName;
+        public String attachmentContentType;
+        public String attachmentUrl;
+
+
+        public void run(){
+            String url = "https://hacknarock.release.commandcentral.com";
+            URL obj = null;
+            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+            Date date = new Date();
+            String acDate = dateFormat.format(date);
+            String actDate = acDate.replace('_','T');
+            String actualDate = actDate.concat(".000Z");
+            System.out.print(actualDate);
+
+            try {
+                obj = new URL(url);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            HttpsURLConnection con = null;
+            try {
+                con = (HttpsURLConnection) obj.openConnection();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            con.setRequestProperty("Authorization", "Basic " + "dmZWdHZnRVI2YjlvN3Zn");
+            con.setRequestProperty("Content-Type", "application/json");
+
+            try {
+                con.setRequestMethod("PUT");
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            }
+            con.setDoOutput(true);
+            DataOutputStream wr = null;
+            try {
+                wr = new DataOutputStream(con.getOutputStream());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            JSONObject js = null;
+            try {
+                js = new JSONObject("{\n" +
+                        " \"metaHeader\":\n" +
+                        " {\n" +
+                        "   \"metaTimeStamp\": \""+actualDate+"\"\n" +
+                        " },\n" +
+                        " \"eventHeader\":\n" +
+                        " {\n" +
+                        "   \"id\": \""+id+"\",\n" +
+                        "   \"label\": \""+label+"\",\n" +
+                        "   \"timeStamp\": \""+actualDate+"\",\n" +
+                        "   \"location\": {\n" +
+                        "     \"latitude\": "+latitude+",\n" +
+                        "     \"longitude\": "+longitude+"\n" +
+                        "   },\n" +
+                        "\"detailedDescription\": \""+description+"\","+
+                        "   \"icon\":\n" +
+                        "   {\n" +
+                        "     \"url\": \"MsiIcon://"+icon+"\"\n" +
+                        "   },\n" +
+                        "   \"expirationTimeStamp\": \"2018-03-11T02:52:56.618Z\",\n" +
+                        "   \"priority\": \""+priority+"\",\n" +
+                        "    \"attachments\": [\n" +
+                        "      {\n" +
+                        "        // Title of Attachment\n" +
+                        "        \"name\": \""+attachmentName+"\",\n" +
+                        "        \"contentType\": \""+attachmentContentType+"\",\n" +
+                        "        \"url\": \""+attachmentUrl+"\"\n" +
+                        "      }\n" +
+                        "    ]" +
+                        " }\n" +
+                        "}");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            try {
+                wr.writeBytes(js.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                wr.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                wr.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            int responseCode = 0;
+            try {
+                responseCode = con.getResponseCode();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("\nSending 'POST' request to URL : " + url);
+            //        System.out.println("Post parameters : " + urlParameters);
+            System.out.println("Response Code : " + responseCode);
+
+            BufferedReader in = null;
+            try {
+                in = new BufferedReader(
+                        new InputStreamReader(con.getInputStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            try {
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            //print result
+            System.out.println(response.toString());
+
+
+        }
+    }
+
+    public void createPUT(String lonitude, String latitude, String id, String label, String description) throws IOException, JSONException {
+        //Dodawanie punktow na mapie
+        PUTThread thread = new PUTThread();
+        thread.longitude=longitude;
+        thread.latitude=latitude;
+        //priorities = ['emergency' | 'high' | 'medium' | 'low' | 'diagnostic' | 'unknown']
+        thread.priority="medium";
+        thread.id=id;
+        thread.label=label;
+        //iconsAvailable https://codepen.io/anon/pen/LQodbY
+        thread.icon="ic_unit_police_sirens";
+        thread.description=description;
+        thread.attachmentName="Incident location";
+        thread.attachmentContentType="application/link";
+        thread.attachmentUrl="https://goo.gl/maps/Yiz4TLDBF3L2";
+
+        thread.start();
+    }
+
+}
+
+
+
 /*      Oblicza dystanse po punktach
     double distanceTo(BoundingBox bbx){
         if(this.isEmpty() || bbx.isEmpty())
@@ -85,4 +256,3 @@ public class Mission {
     }
 
 */
-}
