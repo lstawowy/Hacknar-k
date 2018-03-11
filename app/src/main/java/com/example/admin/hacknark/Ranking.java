b93aa29a4e03c46cb0009a46049dc0f7a9e802e7
Łukasz Stawowy <l.w.stawowy@gmail.com>
2018-03-11 03:32:21 +0100

    design po raz pierwszy

--- app/src/main/java/com/example/admin/hacknark/Ranking.java
+++ app/src/main/java/com/example/admin/hacknark/Ranking.java
@@ -1,10 +1,12 @@
 package com.example.admin.hacknark;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class Ranking extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,7 +14,18 @@ public class Ranking extends AppCompatActivity {
         setContentView(R.layout.activity_ranking);
     }
 
-    public void JumpTo(View view){
+    public void JumpToSettings(View View) {
+        Intent jump = new Intent(this, SettingsActivity.class);
+        startActivity(jump);
+    }
 
+    public void JumpToMain(View View) {
+        Intent jump = new Intent(this, MainActivity.class);
+        startActivity(jump);
+    }
+
+    public void JumpToProfil(View View) {
+        Intent jump = new Intent(this, Profile.class);
+        startActivity(jump);
     }
 }
--- app/src/main/java/com/example/admin/hacknark/SettingsActivity.java
+++ app/src/main/java/com/example/admin/hacknark/SettingsActivity.java
@@ -29,4 +29,19 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
     }
+
+    public void JumpToSettings(View View) {
+        Intent jump = new Intent(this, SettingsActivity.class);
+        startActivity(jump);
+    }
+
+    public void JumpToMain(View View) {
+        Intent jump = new Intent(this, MainActivity.class);
+        startActivity(jump);
+    }
+
+    public void JumpToProfil(View View) {
+        Intent jump = new Intent(this, Profile.class);
+        startActivity(jump);
+    }
 }
